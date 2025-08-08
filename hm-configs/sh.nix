f843c7923eb8b3967f4d421bf91ad34cc2bd37ae
SuperAmoeba <superamoeba.mc@gmail.com>
2025-08-08 21:18:27 +0200

    Added cover art add script for flacs - jpg

--- hm-configs/sh.nix
+++ hm-configs/sh.nix
@@ -13,8 +13,9 @@ let
     my_fetch_lyrics = "~/.dotfiles/hm-configs/musics/utils/my_fetch_album_lyrics.sh ./";
     coverjpg = "~/.dotfiles/hm-configs/musics/utils/album_art_add_jpg.sh";
     coverpng = "~/.dotfiles/hm-configs/musics/utils/album_art_add_png.sh";
-    coverflac = "~/.dotfiles/hm-configs/musics/utils/album_art_add_flac.sh";
-    
+    coverflacjpg = "~/.dotfiles/hm-configs/musics/utils/album_art_add_flac_jpg.sh";
+    coverflacpng = "~/.dotfiles/hm-configs/musics/utils/album_art_add_flac_png.sh";
+
   };
 in
 {
