3fb839d1ffd851b022e117317c73480cdb992eba
SuperAmoeba <superamoeba.mc@gmail.com>
2025-08-08 21:38:33 +0200

    eyeD3 doesn't support flacs :sob:

--- hm-configs/musics/utils/album_art_add_flac_jpg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for f in *.flac; do
-  eyeD3 --add-image=cover.jpg:FRONT_COVER "$f"
-done
--- hm-configs/musics/utils/album_art_add_flac_png.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for f in *.flac; do
-  eyeD3 --add-image=cover.png:FRONT_COVER "$f"
-done
